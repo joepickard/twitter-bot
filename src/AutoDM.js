d8536a7961a0845f538537564a13dfaabcdae186
joepickard <53952800+joepickard@users.noreply.github.com>
2019-08-09 15:43:58 -0400

    Update AutoDM.js

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -41,7 +41,7 @@ const GenerateMessage = name => {
   ];
   const d = new Date();
   const dayName = days[d.getDay()];
-  return `Hi ${name} Thanks for .... \n Happy ${dayName} 😊😊 `; // your message
+  return `Hi ${name} Thanks for the follow! Be Sure to Add us on Instagram and LinkedIn as well or check out our website at iDorSS.com! 😊😊`; // your message
   // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
 };
 
